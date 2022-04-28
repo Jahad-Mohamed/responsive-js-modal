62bfd6ff51785a853d02d9384e86796e23e1b0d9
Jahad Mohamed <jahadmohamed@live.co.uk>
2022-04-28 19:04:04 +0100

    modal open/close functionality working

---
+++ script.js
@@ -0,0 +1,26 @@
+"use strict";
+
+const modal = document.querySelector(".modal");
+const overlay = document.querySelector(".overlay");
+const btnCloseModal = document.querySelector(".close-modal");
+const btnOpenModal = document.querySelectorAll(".show-modal");
+
+const openModal = function () {
+  modal.classList.remove("hidden");
+  overlay.classList.remove("hidden");
+};
+// open modal with overlay effect
+for (let i = 0; i < btnOpenModal.length; i++) {
+  btnOpenModal[i].addEventListener("click", openModal);
+}
+
+// close modal when clicked
+const closeModal = function () {
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
+};
+
+btnCloseModal.addEventListener("click", closeModal);
+
+// close modal when clicked
+overlay.addEventListener("click", closeModal);
