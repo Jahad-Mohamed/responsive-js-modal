2c2520a65963c57e9f6c7672e1311f036d707d22
Jahad Mohamed <jahadmohamed@live.co.uk>
2022-04-29 09:17:20 +0100

    js modal complete

--- script.js
+++ script.js
@@ -9,6 +9,7 @@ const openModal = function () {
   modal.classList.remove("hidden");
   overlay.classList.remove("hidden");
 };
+
 // open modal with overlay effect
 for (let i = 0; i < btnOpenModal.length; i++) {
   btnOpenModal[i].addEventListener("click", openModal);
@@ -24,3 +25,13 @@ btnCloseModal.addEventListener("click", closeModal);
 
 // close modal when clicked
 overlay.addEventListener("click", closeModal);
+
+// close modal with escape key
+
+document.addEventListener("keydown", function (e) {
+  console.log(e.key);
+
+  if (e.key === "Escape" && !modal.classList.contains("hidden")) {
+    closeModal();
+  }
+});
